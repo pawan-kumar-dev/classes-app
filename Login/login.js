2a75dc720ca247e69993cee182f1c4f7e1fa0a87
pawan-kumar-dev <pawan.kv612@gmail.com>
2020-10-28 21:36:52 +0530

    login added

---
+++ Login/login.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("admin");
+var y = document.getElementById("teacher");
+var z = document.getElementById("btn");
+
+function teacher() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+}
+
+function admin() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+}
